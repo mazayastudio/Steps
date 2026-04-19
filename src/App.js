e96bf599f3ea1c15e2c8a2eef365cba8da9099d8
sayudhalw <mazaya.studio@outlook.com>
2026-04-19 17:44:27 +0700

    Add `.idea` configuration files and update React App with step navigation functionality

--- src/App.js
+++ src/App.js
@@ -7,19 +7,42 @@ const messages = [
 export default function App() {
   const step = 1;
 
+  function handlePrevious() {
+    alert("Previous");
+  }
+
+  function handleNext() {
+    alert("Next");
+  }
+
   return (
     <div className="steps">
-      <div className='numbers'>
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
 
-      <p className="message">Step {step}: {messages[step - 1]}</p>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
 
-      <div className='buttons'>
-        <button style={{backgroundColor: '#7950f2', color: '#ffffff'}}>Previous</button>
-        <button style={{backgroundColor: '#7950f2', color: '#ffffff'}}>Next</button>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#ffffff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{
+            backgroundColor: "#7950f2",
+            color: "#ffffff",
+          }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
